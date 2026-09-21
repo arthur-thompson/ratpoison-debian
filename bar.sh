77c44c519afe0d16a65c8fcd26b420278d2f8a43
Arthur Thompson <102389665+arthur-thompson@users.noreply.github.com>
2026-09-22 01:37:47 +1200

    Add files via upload

---
+++ bar.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env sh
+
+MAINFONT="-*-fixed-medium-*-*-*-14-*-*-*-*-*-*-*"
+BARHEIGHT=16
+FGCOLOR="#000000"
+BGCOLOR="#d3d3d3"
+
+clock() {
+	TIME=`date +"%a %d %b %Y %I:%M %p"`
+	echo "$TIME"
+}
+
+if pgrep lemonbar > /dev/null; then
+    killall lemonbar
+fi
+
+while true; do
+
+	panel_layout="%{c}$(clock)"
+        echo "${panel_layout} "
+
+	sleep 5
+done | lemonbar -d -g x"${BARHEIGHT}" -B "${BGCOLOR}" -F "${FGCOLOR}" -f "$MAINFONT" | sh > /dev/null 2>&1
